0c9e355f7878c7e4407846785dd93331e5196f0d
Jason Bruno Dsouza <106374595+Jsn99@users.noreply.github.com>
2023-02-06 17:54:43 +0530

    Update index.js

--- Menu/index.js
+++ Menu/index.js
@@ -5,7 +5,7 @@ const menu = [
     category: "breakfast",
     price: 15.99,
     img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `Buttermilk Pancakes are wonderfully thick, have a light and spongy texture and flavor that only hints of the richness and tanginess that buttermilk offers `,
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const menu = [
     category: "lunch",
     price: 13.99,
     img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `Diner a map that encourages easy navigation between hunger and satisfaction `,
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const menu = [
     category: "shakes",
     price: 6.99,
     img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `Godzailla Milkshake blended with banana & mint chocolate chip ice cream, drizzled with melted chocolate & topped with more ice cream`,
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const menu = [
     category: "breakfast",
     price: 20.99,
     img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `A bread recipe that can be adapted to become a complete meal is that of a bread omelette. Creamy eggy filling, seasoned with scintillating flavours and finely chopped vegetables, is sandwiched between gloriously toasted breads that contain all the goodness of butter and carbs`,
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const menu = [
     category: "lunch",
     price: 22.99,
     img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `A burger consisting of one or more cooked patties of ground meat, usually meat, placed inside a sliced bread roll or bun `,
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const menu = [
     category: "shakes",
     price: 18.99,
     img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `Oreo is a brand of sandwich cookie consisting of two biscuits or cookie pieces with a sweet creme filling`,
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ const menu = [
     category: "breakfast",
     price: 8.99,
     img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `Bacon is a type of salt-cured pork made from various cuts, typically the belly or less fatty parts of the back`,
   },
   {
     id: 8,
@@ -61,7 +61,7 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: ` Hamburgers are based on traditional German dishes, but in their modern popular form they can be reasonably considered American dishes`,
   },
   {
     id: 9,
@@ -69,15 +69,15 @@ const menu = [
     category: "shakes",
     price: 16.99,
     img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `A milkshake is a sweet beverage made by blending milk, ice cream, and flavorings or sweeteners such as butterscotch, caramel sauce, chocolate syrup, fruit syrup, or whole fruit into a thick, sweet, cold mixture`,
   },
   {
     id: 10,
-    title: "bison steak",
+    title: "Biryani",
     category: "dinner",
     price: 22.99,
     img: "./images/item-10.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `A world-renowned Indian dish, biryani takes time and practice to make but is worth every bit of the effort`,
   },
 ];
 // get parent element
