2fa82ef02d3fb73582cbd29e3b2d190866d16187
Jason Bruno Dsouza <106374595+Jsn99@users.noreply.github.com>
2023-03-09 20:38:59 +0530

    Update script.js

--- CountDown GiveAway/script.js
+++ CountDown GiveAway/script.js
@@ -23,7 +23,7 @@ const weekdays = [
 ];
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
-//we can choose only h4 from the div
+//To choose only h4 from the div
 const items = document.querySelectorAll(".deadline-format h4");
 
 let tempDate = new Date();
